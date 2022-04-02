f174367f1f55a3107645305a6724458707d9c1fc
bcarlee <bcarlee@student.21-school.ru>
2022-04-02 13:27:47 +0300

    task 1

---
+++ CPP 00/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int main(int ac, char **av)
+{
+	if (ac == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+	else
+	{
+		for (int i = 1; i < ac; i++) {
+			std::string str(av[i]);
+			for (size_t j = 0; j < str.length(); j++)
+			{
+				std::cout << (char)std::toupper(str[j]);
+			}
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
