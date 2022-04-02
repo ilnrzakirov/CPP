342bf2d3e93ee1431d19fb5c49eed7a0ff55abd7
bcarlee <bcarlee@student.21-school.ru>
2022-04-02 16:21:51 +0300

    task 2

--- CPP 00/ex01/Contact.hpp
+++ CPP 00/ex01/Contact.hpp
@@ -3,7 +3,27 @@
 
 # include <iostream>
 
-class Contact{
+class	Contact {
+
+private:
+	std::string	first_name; // std - пространство имен STD, string - класс
+	// с методами для работы со строками,  first_name -объект класса string
+	std::string	last_name;
+	std::string	nickname;
+	std::string	phone_number;
+	std::string	darkest_secret;
+
+public:
+	Contact();
+	Contact(std::string first_name,
+			std::string last_name,
+			std::string nickname,
+			std::string phone_number,
+			std::string darkest_secret);
+	std::string get_first_name();
+	std::string get_last_name();
+	std::string get_nickname();
+	~Contact();
 
 };
 
