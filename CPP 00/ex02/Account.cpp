3f13267228ca4eafb56b38fe36d3ca25baa5a009
bcarlee <bcarlee@student.21-school.ru>
2022-04-09 12:58:28 +0300

    task 3

--- CPP 00/ex02/Account.cpp
+++ CPP 00/ex02/Account.cpp
@@ -25,8 +25,18 @@ int Account::getNbWithdrawals(){
 
 static void	displayAccountsInfos(){
 	time_t now;
-	struct tm ptr;
+	struct tm *ptr;
 
 	time(&now);
 	ptr = localtime(&now);
+	std::cout << "[" << std::setfill('0')
+		 << std::setw(4) << ptr->tm_year + 1900
+		 << std::setw(2) << ptr->tm_mon + 1
+		 << std::setw(2) << ptr->tm_mday
+		 << "_"
+		 << std::setw(2) << ptr->tm_hour
+		 << std::setw(2) << ptr->tm_min
+		 << std::setw(2) << ptr->tm_sec
+		 << std::setw(1)
+		 << "]";
 }
