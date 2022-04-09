26f8f89461682425a056fa666677e3912e34a3f4
bcarlee <bcarlee@student.21-school.ru>
2022-04-09 10:12:31 +0300

    task 2

--- CPP 00/ex01/PhoneBook.cpp
+++ CPP 00/ex01/PhoneBook.cpp
@@ -12,11 +12,11 @@ PhoneBook:: ~PhoneBook() //деструктор
 void PhoneBook::add_contact(void) {
 
 	if (this->number == 8) {
-		this->contact[0].create_contact();
+		this->contact[0].setData();
 	}
 	else
 	{
-		this->contact[this->number + 1] = contact;
+		this->contact[this->number + 1].setData();
 	}
 }
 
@@ -55,4 +55,35 @@ void PhoneBook::viewContacts() const
 		std::cout << "\n";
 	}
 	std::cout << "+--------------------------------------+\n";
+}
+
+void    PhoneBook::getMan()
+{
+	std::cout << "+-------------------------------------------------------+\n";
+	std::cout << "|                  AVAILABLE COMMANDS                   |\n";
+	std::cout << "+-------------------------------------------------------+\n";
+	std::cout << "|                                                       |\n";
+	std::cout << "|     ADD     -   Add a contact                         |\n";
+	std::cout << "|                                                       |\n";
+	std::cout << "|     SEARCH  -   Find a contact                        |\n";
+	std::cout << "|                                                       |\n";
+	std::cout << "|     EXIT    -   exit from PhoneBook                   |\n";
+	std::cout << "|                                                       |\n";
+	std::cout << "+-------------------------------------------------------+\n";
+}
+
+void	PhoneBook::NumContact(void) const
+{
+	std::string s_id;
+	int	index;
+
+	std::cout << "Enter num contact:\n";
+	(void)getline(std::cin, s_id);
+	index = atoi(s_id.c_str());
+	if (index <= 0 || index > this->cntContact)
+	{
+		std::cout << "Contact not found :(\n";
+		return ;
+	}
+	viewFullInfo(contacts[index - 1]);
 }
--- CPP 00/ex01/PhoneBook.hpp
+++ CPP 00/ex01/PhoneBook.hpp
@@ -1,17 +1,20 @@
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
 
+# include <stdlib.h>
 # include "./Contact.hpp"
 
 class PhoneBook{
 private:
 	Contact contact[8];
 	int number;
-	void	viewContacts() const;
+	void viewContacts() const;
+	void NumContact() const;
 
 public:
 	PhoneBook();
 	void add_contact();
+	void getMan();
 	void search();
 	void exit();
 	~PhoneBook();
