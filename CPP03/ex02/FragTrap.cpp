e65d8ab0539628d7d52b42ed178f3a9f3c27a5e7
bcarlee <bcarlee@student.21-school.ru>
2022-05-30 14:22:18 +0300

    ex02

--- CPP03/ex02/FragTrap.cpp
+++ CPP03/ex02/FragTrap.cpp
@@ -1,2 +1,16 @@
 #include "FragTrap.hpp"
 
+FragTrap::FragTrap() {
+	this->hp = 100;
+	this->energy = 100;
+	this->attackDamage = 30;
+	std::cout << "the FragTrap constructor is called\n";
+}
+
+FragTrap::FragTrap(std::string name) {
+	this->name = name;
+	this->hp = 100;
+	this->energy = 100;
+	this->attackDamage = 30;
+	std::cout << "the " << this->getName() << "constructor is called\n";
+}
