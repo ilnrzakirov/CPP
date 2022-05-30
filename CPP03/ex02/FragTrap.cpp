eb7572dd2df2fd64ec665f9e70163c88107b055a
bcarlee <bcarlee@student.21-school.ru>
2022-05-30 14:32:10 +0300

    ex02

--- CPP03/ex02/FragTrap.cpp
+++ CPP03/ex02/FragTrap.cpp
@@ -14,3 +14,26 @@ FragTrap::FragTrap(std::string name) {
 	this->attackDamage = 30;
 	std::cout << "the " << this->getName() << "constructor is called\n";
 }
+
+FragTrap::~FragTrap() {
+	std::cout << "the FragTrap destructor from " << this->getName() << "is "
+																	   "called\n";
+}
+
+FragTrap::FragTrap(const FragTrap &fragTrap) {
+	std::cout << "Copy constructor is called\n";
+	*this = fragTrap;
+}
+
+FragTrap &FragTrap::operator=(const FragTrap &fragTrap) {
+	std::cout <<"Copy assignment operator called\n";
+	this->name = other.getName();
+	this->hp = other.getHp();
+	this->energy = other.getEnergy();
+	this->attackDamage = other.getAttackDamage();
+	return  (*this);
+}
+
+void FragTrap::highFivesGuys() {
+	std::cout << this->name << " give me a five!\n";
+}
