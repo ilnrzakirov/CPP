30f099514c20bfb4cec2294c1774004ff68f6d43
bcarlee <bcarlee@student.21-school.ru>
2022-05-31 12:22:05 +0300

    ex03

--- CPP04/ex00/Animal.hpp
+++ CPP04/ex00/Animal.hpp
@@ -6,5 +6,11 @@
 class Animal {
 private:
 	std::string type;
+public:
+	void makeSound();
+	Animal();
+	~Animal();
+	Animal &operator=(const Animal animal);
+	Animal(const Animal &animal);
 };
 #endif
