50f10d5d6588aa087e75f237576b71f0a2442aa9
bcarlee <bcarlee@student.21-school.ru>
2022-06-01 12:47:49 +0300

    ex00

---
+++ CPP05/ex00/Bureaucrat.cpp
@@ -0,0 +1,6 @@
+#include "Bureaucrat.hpp"
+
+
+class Bureaucrat {
+
+};
---
+++ CPP05/ex00/Bureaucrat.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Becki Carlee on 6/1/22.
+//
+
+#ifndef CPP_BUREAUCRAT_HPP
+#define CPP_BUREAUCRAT_HPP
+
+
+class Bureaucrat {
+
+};
+
+
+#endif //CPP_BUREAUCRAT_HPP
