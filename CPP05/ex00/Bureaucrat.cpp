1e0561fcc2b15d7dc2b0959509f5f271fa5ed3a5
bcarlee <bcarlee@student.21-school.ru>
2022-06-01 16:03:24 +0300

    ex00

--- CPP05/ex00/Bureaucrat.cpp
+++ CPP05/ex00/Bureaucrat.cpp
@@ -1,15 +1,4 @@
 #include "Bureaucrat.hpp"
 
 
-class Bureaucrat {
-protected:
-	std::string name;
-	int grade;
-public:
-	Bureaucrat();
-	Bureaucrat(std::string name, int grade);
-	~Bureaucrat()
-	Bureaucrat(const Bureaucrat &bureaucrat);
-	Bureaucrat &Bureaucrat::operator=(const Bureaucrat bureaucrat);
-
-};
+#endif
--- CPP05/ex00/Bureaucrat.hpp
+++ CPP05/ex00/Bureaucrat.hpp
@@ -2,13 +2,39 @@
 // Created by Becki Carlee on 6/1/22.
 //
 
-#ifndef CPP_BUREAUCRAT_HPP
-#define CPP_BUREAUCRAT_HPP
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+#include <exception>
 
 
 class Bureaucrat {
+private:
+	std::string name;
+	int grade;
+public:
+	Bureaucrat();
+	Bureaucrat(std::string name, int grade);
+	~Bureaucrat()
+	Bureaucrat(const Bureaucrat &bureaucrat);
+	Bureaucrat &Bureaucrat::operator=(const Bureaucrat bureaucrat);
+	std::string getName() const;
+	int getGrade() const;
+	void increment();
+	void decrement();
 
+	class GradeTooHighException : std::exception {
+	public: GradeTooHighException(std::string error) : std::runtime_error
+	(error);
+	};
+
+	class GradeTooLowException : std::exception {
+	public: GradeTooLowException(std::string error) : std::runtime_error(error);
+	};
 };
 
 
-#endif //CPP_BUREAUCRAT_HPP
+
+#endif
