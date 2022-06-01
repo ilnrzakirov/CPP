ca94d4966e8c12a63cb98c834421d74846c7cf9d
bcarlee <bcarlee@student.21-school.ru>
2022-06-01 12:51:19 +0300

    ex00

--- CPP05/ex00/Bureaucrat.cpp
+++ CPP05/ex00/Bureaucrat.cpp
@@ -2,5 +2,14 @@
 
 
 class Bureaucrat {
+protected:
+	std::string name;
+	int grade;
+public:
+	Bureaucrat();
+	Bureaucrat(std::string name, int grade);
+	~Bureaucrat()
+	Bureaucrat(const Bureaucrat &bureaucrat);
+	Bureaucrat &Bureaucrat::operator=(const Bureaucrat bureaucrat);
 
 };
