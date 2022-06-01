baea5b8f9c9ad5888e2d0108461a3f8c80bdd92e
bcarlee <bcarlee@student.21-school.ru>
2022-06-01 17:42:00 +0300

    ex01

--- CPP05/ex01/Form.hpp
+++ CPP05/ex01/Form.hpp
@@ -5,14 +5,32 @@
 #ifndef CPP_FORM_HPP
 #define CPP_FORM_HPP
 
+#include "Bureaucrat.hpp"
+
 
 class Form {
 private:
 	std::string name;
-	bool sign;
+	bool is_sign;
 	int grade_to_sign;
 	int grade_to_execute;
-};
+public:
+	class GradeTooHighException : public std::runtime_error {
+	public: GradeTooHighException(const std::string &error) : std::runtime_error
+																	  (error) {};
+	};
 
+	class GradeTooLowException : public std::runtime_error {
+	public: GradeTooLowException(const std::string &error) : std::runtime_error
+																	 (error) {};
+	};
+	Form();
+	Form(std::string name, int grade_to_sign, int grade_to_execute );
+	~Form();
+	Form(const Form &form);
+	Form &operator=(const Form form);
+	void beSigned(Bureaucrat &bureaucrat);
+};
+std::ostream &operator<< (std::ostream &os, const Form &form);
 
 #endif //CPP_FORM_HPP
