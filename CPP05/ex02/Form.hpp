1e0d0d373b0e802b9eb2a422f5e79902f50c5605
bcarlee <bcarlee@student.21-school.ru>
2022-06-02 09:39:59 +0300

    ex02

---
+++ CPP05/ex02/Form.hpp
@@ -0,0 +1,41 @@
+//
+// Created by Becki Carlee on 6/1/22.
+//
+
+#ifndef CPP_FORM_HPP
+#define CPP_FORM_HPP
+
+#include "Bureaucrat.hpp"
+
+class Bureaucrat;
+
+class Form {
+private:
+	std::string name;
+	bool is_sign;
+	int grade_to_sign;
+	int grade_to_execute;
+public:
+	class GradeTooHighException : public std::runtime_error {
+	public: GradeTooHighException(const std::string &error) : std::runtime_error
+																	  (error) {};
+	};
+
+	class GradeTooLowException : public std::runtime_error {
+	public: GradeTooLowException(const std::string &error) : std::runtime_error
+																	 (error) {};
+	};
+	Form();
+	Form(std::string name, int grade_to_sign, int grade_to_execute );
+	~Form();
+	Form(const Form &form);
+	Form &operator=(const Form form);
+	void beSigned(Bureaucrat &bureaucrat);
+	int getGradeToSign() const;
+	int getGradeToExecute() const;
+	std::string getName() const;
+	std::string getSigned() const;
+};
+std::ostream &operator<< (std::ostream &os, const Form &form);
+
+#endif //CPP_FORM_HPP
