9496c0083204a4f1c032d0a27628c83f31d9d7b0
bcarlee <bcarlee@student.21-school.ru>
2022-06-05 16:54:04 +0300

    ex02

--- CPP07/ex02/Array.cpp
+++ CPP07/ex02/Array.cpp
@@ -4,4 +4,18 @@
 
 #include "Array.hpp"
 
+template<class T>
+Array<T>::Array() : arr(0), size(0) {}
+
+template<class T>
+Array<T>::Array(unsigned int n) {
+	this->arr = new T[n];
+	this->size = n;
+}
+
+template<class T>
+Array<T>::~Array<T>() {
+	delete []this->arr;
+}
+
 
