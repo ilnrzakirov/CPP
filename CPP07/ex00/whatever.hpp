781326b6001a779bbd7ff650a6d9a7ae340a2fae
bcarlee <bcarlee@student.21-school.ru>
2022-06-05 14:57:57 +0300

    ex00

---
+++ CPP07/ex00/whatever.hpp
@@ -0,0 +1,37 @@
+//
+// Created by Becki Carlee on 6/5/22.
+//
+
+#ifndef CPP_WHATEVER_HPP
+#define CPP_WHATEVER_HPP
+
+
+#include <iostream>
+#include <string>
+
+template <class T>
+T min(const T &a, const T &b){
+	if (a < b)
+		return a;
+	else if (a >b)
+		return b;
+	else
+		return b;
+}
+
+template <class T>
+T max(const T &a, const T &b){
+	if (a > b)
+		return a;
+	else if (a < b)
+		return b;
+	else
+		return b;
+}
+
+template <class T>
+T swap(T &a, T &b){
+
+}
+
+#endif //CPP_WHATEVER_HPP
