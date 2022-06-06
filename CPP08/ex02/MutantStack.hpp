618a4be7049c5a554ab7597d9e49e2a87e50fc6a
bcarlee <bcarlee@student.21-school.ru>
2022-06-06 13:05:37 +0300

    ex01

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -7,10 +7,12 @@
 #include <iostream>
 #include <string>
 #include <stack>
+#include <algorithm>
 
 template <class T>
 class MutantStack : std::stack<T> {
 public:
+	typedef typename std::stack<T>::container_type::iterator iterator;
 		MutantStack();
 		~MutantStack();
 		MutantStack(const MutantStack<T> &mutantStack) : std::stack<T>
@@ -18,6 +20,14 @@ public:
 		MutantStack const &operator=(MutantStack<T> &mutantStack){
 			std::stack<T>::operator=(mutantStack);
 			return (*this);
+		}
+
+		iterator begin(){
+			return this->c.begin();
+		}
+
+		iterator end(){
+			return this->c.end();
 		}
 };
 
