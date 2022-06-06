1490dd3e8589a706d63d9ee18258f12e360fbefd
bcarlee <bcarlee@student.21-school.ru>
2022-06-06 10:09:17 +0300

    ex00

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -4,6 +4,16 @@
 #include <iostream>
 #include <string>
 
+template <class T>
+int easyfind(T &t, int i){
+	T::itertor itertor;
+	itertor = std::find(t.begin(), t.end(), i);
 
+	if (itertor != te.end){
+		return *itertor;
+	} else {
+		throw std::runtime_error("Not found");
+	}
+}
 
 #endif
